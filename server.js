86f913db73ac9f984c7392f01d2ebfa6cc5da2ba
Ramon Oliveira <ramon.ricardo13@gmail.com>
2023-05-08 19:50:23 -0300

    att

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-/*const app = require ('./src/app')
+const app = require ('./src/app')
 const PORT = 3000
 
 // 3000, 6000, 6060, 8080, 8088
 
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))*/
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
